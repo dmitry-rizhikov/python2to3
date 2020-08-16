e9aa9fa2a320100ed07d09d8ed68dd916fc47742
Dmitry Rizhikov <dmitryrijikov@gmail.com>
2020-08-16 09:50:06 +0200

    fix test

--- test_path_utils.py
+++ test_path_utils.py
@@ -36,7 +36,7 @@ class Test(TestCase):
             file.write("file2")
 
     def test_complete_scenario(self):
-        self.path_utils.add_folders_to_path(dirname='folder')
+        self.path_utils.add_folders_to_path(dirname='folder', exclude_dirs=[])
 
         self.assertIsNotNone(sys.path.index('folder'))
         self.assertIsNotNone(sys.path.index('folder/subfolder1'))
@@ -44,13 +44,13 @@ class Test(TestCase):
         self.assertIsNotNone(sys.path.index('folder/subfolder1/xxx'))
         self.assertIsNotNone(sys.path.index('folder/subfolder2/yyy'))
 
-        self.pylint2.run_pylint(dirname='folder', out_file='pylint2_old.txt')
+        self.pylint2.run_pylint(dirname='folder', out_file='pylint2_old.txt', exclude_dirs=[])
 
         self.assertGreater(os.path.getsize('pylint2_old.txt'), 0)
 
-        self.futurize.run_futurize('folder')
+        self.futurize.run_futurize(dirname='folder', exclude_dirs=[], out_file='futurize.txt')
 
-        self.pylint2.run_pylint(dirname='folder', out_file='pylint2_new.txt')
+        self.pylint2.run_pylint(dirname='folder', out_file='pylint2_new.txt', exclude_dirs=[])
 
         self.assertGreater(os.path.getsize('pylint2_new.txt'), 0)
 
@@ -58,7 +58,7 @@ class Test(TestCase):
 
         self.assertGreater(os.path.getsize('pylint2_diff.txt'), 0)
 
-        self.pylint3.run_pylint(dirname='folder', out_file='pylint3_new.txt')
+        self.pylint3.run_pylint(dirname='folder', out_file='pylint3_new.txt', exclude_dirs=[])
 
         self.diff.diff_files(from_file='pylint2_new.txt', to_file='pylint3_new.txt', out_file='pylint3_diff.txt')
 

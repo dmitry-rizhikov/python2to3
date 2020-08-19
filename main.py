5ac74d3ab3c5222a817d806b8567b79043b0b17c
Dmitry Rizhikov <dmitryrijikov@gmail.com>
2020-08-19 12:54:54 +0200

    fix config.yml

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from copy_directory import Copy
 from diff_files import Diff
+from read_config_yml import Config
 from utils import Utils
 from run_futurize import Futurize
 from run_pylint import Pylint
@@ -21,25 +22,24 @@ if __name__ == '__main__':
     copy = Copy()
 
     logging.basicConfig(level=logging.INFO, stream=sys.stderr)
-
-    path = '/home/dmitry/zeev-stxnet/Tools_new'
-    exclude = ['External', 'AutoTestVW/VWAutomatedTesting/WorkspaceEnum/Lib']
     
-    new_path = copy.copy_dir(path)
+    config = Config('config.yml')
 
-    utils.add_folders_to_path(dirname=new_path, exclude_dirs=exclude)
+    new_path = copy.copy_dir(config.get_path())
 
-    pylint2.run_pylint(dirname=new_path, out_file='pylint2_old.txt', exclude_dirs=exclude)
+    utils.add_folders_to_path(dirname=new_path, exclude_dirs=config.get_excludes())
 
-    futurize.run_futurize(dirname=new_path, out_file='futurize.txt', exclude_dirs=exclude)
+    pylint2.run_pylint(dirname=new_path, out_file='pylint2_old.txt', exclude_dirs=config.get_excludes())
+
+    futurize.run_futurize(dirname=new_path, out_file='futurize.txt', exclude_dirs=config.get_excludes())
 
     futurize.validate_output(file='futurize.txt')
 
-    pylint2.run_pylint(dirname=new_path, out_file='pylint2_new.txt', exclude_dirs=exclude)
+    pylint2.run_pylint(dirname=new_path, out_file='pylint2_new.txt', exclude_dirs=config.get_excludes())
 
     diff.diff_files(from_file='pylint2_old.txt', to_file='pylint2_new.txt', out_file='pylint2_diff.txt')
 
-    pylint3.run_pylint(dirname=new_path, out_file='pylint3_new.txt', exclude_dirs=exclude)
+    pylint3.run_pylint(dirname=new_path, out_file='pylint3_new.txt', exclude_dirs=config.get_excludes())
 
     diff.diff_files(from_file='pylint2_new.txt', to_file='pylint3_new.txt', out_file='pylint3_diff.txt')
     
